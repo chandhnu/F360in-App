367038d9a20e255b9248da0a32ec5e2a0705257d
chandhnu <chandhnu@gmail.com>
2026-08-20 23:11:15 +0530

    Update logger.dart

--- lib/utils/logger.dart
+++ lib/utils/logger.dart
@@ -11,13 +11,19 @@ class AppLogger {
     ),
   );
 
-  static void debug(String message) => _logger.d(message);
-
-  static void warning(String message) => _logger.w(message);
-
-  static void error(String message, [dynamic error, StackTrace? stackTrace]) {
-    _logger.e(message, error: error, stackTrace: stackTrace);
+  static void debug(String message) {
+    _logger.d(message);
   }
 
-  static void info(String message) => _logger.i(message);
+  static void warning(String message) {
+    _logger.w(message);
+  }
+
+  static void error(String message) {
+    _logger.e(message);
+  }
+
+  static void info(String message) {
+    _logger.i(message);
+  }
 }

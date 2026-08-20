4b977edf6c541b7f906a6c26eba5f2dc99ae9284
chandhnu <chandhnu@gmail.com>
2026-08-20 23:17:03 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'config/routes.dart';
 import 'config/themes.dart';
-import 'utils/logger.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();

472e485f1dc2c57781333484bc48722d12e703c6
chandhnu <chandhnu@gmail.com>
2026-08-20 13:26:09 +0530

    Update logger.dart

--- lib/utils/logger.dart
+++ lib/utils/logger.dart
@@ -5,8 +5,11 @@ class AppLogger {
     printer: PrettyPrinter(methodCount: 2, errorMethodCount: 8, lineLength: 120, colors: true, printEmojis: true),
   );
 
+  static void debug(String message) => _logger.d(message);
+  
+  static void warning(String message) => _logger.w(message);
+  
+  static void error(String message, {dynamic stackTrace}) => _logger.e(message, stackTrace: stackTrace);
+  
   static void info(String message) => _logger.i(message);
-  static void debug(String message, [dynamic error, StackTrace? stackTrace]) => _logger.d(message, error: error, stackTrace: stackTrace);
-  static void warning(String message, [dynamic error]) => _logger.w(message, error: error);
-  static void error(String message, [dynamic error, StackTrace? stackTrace]) => _logger.e(message, error: error, stackTrace: stackTrace);
 }
